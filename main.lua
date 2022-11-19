956bb1cf9a01e66b4e497a5f4e647e1deeb591cd
Diogo Costa Hutner <diogohutner@gmail.com>
2022-11-19 17:25:15 -0300

    .
    .

--- main.lua
+++ main.lua
@@ -70,30 +70,18 @@ function Position.Down()
     Position.y = Position.y - 1
 end
 
+function quebrar_frente()
+    andou = false
+    while (not andou) do
+        quebrou = turtle.dig()
+        andou = turtle.forward()
+        if not quebrou then
+            error()
+        end
+    end
+end
 
-print("Digite a coordenada do Eixo X")
--- eixox = tonumber(read())
-eixox = 0
-print("Digite a coordenada do Eixo Y")
--- eixoy = tonumber(read())
-eixoy = 0
-print("Digite a coordenada do Eixo Z")
--- eixoz = tonumber(read())
-eixoz = 0
-print("Digite a orientação, 0 para Norte, 1 East, 2 Sul, 3 West")
--- orient = tonumber(read()) 
-orient = 0
-
-GPS = Position
-GPS.x= eixox
-GPS.y= eixoy
-GPS.z= eixoz
-GPS.orientation= orient
--- orientation : 0 Norte, 1 - East, 2 - Sul, - 3 West
-limitex = eixox - 15
-limitez = eixoz - 15
-
-while true do 
+function work()
     combustivel = turtle.getFuelLevel()
     if (combustivel == 0) then
         reabastecerCombustivel()
@@ -102,66 +90,55 @@ while true do
     if (quantidade_ultimo_slot > 0) then
         esvaziarInventario()
     end
-    if (GPS.x == limitex and GPS.z == limitez) then
-        turtle.turnRight()
-        GPS.TurnRight()
-        for i=1,15 do
-            turtle.forward()
-            GPS.forward()
-        end
-        GPS.turnRight()
-        turtle.turnRight()
-        for i=1,15 do
-            turtle.forward()
-            GPS.forward()
-        end
-        turtle.digDown()
-        turtle.down()
-        turtle.digDown()
-        turtle.down()
-        GPS.Down()
-        GPS.Down()
-        turtle.turnLeft()
-        GPS.TurnLeft()
-        turtle.turnLeft()
-        GPS.TurnLeft()
-    end
-    if (GPS.orientation == 0 and GPS.x == limitex) then
-        turtle.turnLeft()
-        GPS.TurnLeft()
-        turtle.dig()
-        turtle.forward()
-        GPS.forward()
-        turtle.digUp()
-        turtle.digDown()
-        turtle.turnLeft()
-        GPS.TurnLeft()
-    end
-    if (GPS.orientation == 2 and GPS.x == eixox) then
-        turtle.turnRight()
-        GPS.TurnRight()
-        turtle.dig()
-        turtle.forward()
-        GPS.forward()
-        turtle.digUp()
-        turtle.digDown()
-        turtle.turnRight()
-        GPS.turnRight()
-    end
-    
     andou = false
     quebrou = nil
     desvio = false
-    while (not andou) do
-        quebrou = turtle.dig()
-        andou = turtle.forward()
-        if not quebrou then
-            desvio = true
-            break
-        end
-    end
+    
     turtle.digUp()
     turtle.digDown()
-    GPS.forward()
-
 end
+
+function virar( curva )
+    if curva == 1 then
+        turtle.turnLeft()
+        andou = turtle.forward()
+        quebrar_frente()
+        turtle.turnLeft()
+        turtle.digUp()
+        turtle.digDown()
+        curva = 2
+    elseif curva == 2 then
+        turtle.turnRight()
+        andou = turtle.forward()
+        quebrar_frente()
+        turtle.turnRight()
+        turtle.digUp()
+        turtle.digDown()
+        curva = 1
+    end
+    return curva
+end
+
+function descer()
+    for i=1,3 do
+        turtle.digDown()
+        turtle.down()
+    end
+end
+
+while true do 
+    for j=1,14 do
+        for i=1,13 do
+            work()
+        end
+    curva = virar(curva)
+    end
+    turtle.turnRight()
+    for z=1,14 do
+        turtle.forward()
+    end
+    turtle.turnLeft() 
+    descer()
+end
+
+
